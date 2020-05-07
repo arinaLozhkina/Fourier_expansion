9b8e3c106fefb71c5c0fb4bbb00dd972382941c5
arinaLozhkina <32067956+arinaLozhkina@users.noreply.github.com>
2020-05-07 22:30:04 +0300

    Update fourrier_coeff.cpp

--- fourrier_coeff.cpp
+++ fourrier_coeff.cpp
@@ -1,28 +1,24 @@
 #include "fourrier.h"
 
-using namespace GiNaC;
-
-int fourrier_parallel(symbol x, ex input_expr, float int_start, float int_end) {
-    int size, rank; // Ранк и число исполнителей
-    int n = 100; // Число выделяемых гармоник
-    float l = (int_end - int_start) / 2;
-    float cur_cos, cur_sin;
-    Digits = 5; // Константа, определяющая точность
-    float precision = 0.1; // Точность, при которой коэффициенты не считаем равными 0
+int fourrier_parallel(Integral_arguments args){
+    float len = (args.int_end - args.int_start) / 2;
+    float a_0 = compute_integral(args.n_int, args.input_expr, args.x, -len, len, args.rank, args.size) / len;
+    GiNaC::ex result = a_0 / 2;
     
-    float a_0 = compute_integral(input_expr, x, -l, l) / l;
-    ex result = a_0 / 2;
-
-    for (int i = 1; i <= n; i++){
-        cur_cos = compute_integral(input_expr * cos(3.141592 * i * x / l), x, -l, l) / l;
-        cur_sin = compute_integral(input_expr * sin(3.141592 * i * x / l), x, -l, l) / l;
-        if (abs(cur_cos) > precision)
-            result += cur_cos * cos(Pi * i * x / l);
-        if (abs(cur_sin) > precision)
-            result += cur_sin * sin(Pi * i * x / l);
+    float cur_cos, cur_sin;
+    for (int i = 1; i <= args.n; i++){
+        cur_cos = compute_integral(args.n_int, args.input_expr * cos(3.141592 * i * args.x / len), args.x, -len, len, args.rank, args.size) / len;
+        cur_sin = compute_integral(args.n_int, args.input_expr * sin(3.141592 * i * args.x / len), args.x, -len, len, args.rank, args.size) / len;
+        if (abs(cur_cos) > args.precision)
+            result += cur_cos * cos(GiNaC::Pi * i * args.x / len);
+        if (abs(cur_sin) > args.precision)
+            result += cur_sin * sin(GiNaC::Pi * i * args.x / len);
     }
-    std::cout << "Result = " << result.simplify_indexed().evalf().expand() << std::endl;
-    std::cout << "In point = " << result.simplify_indexed().evalf().subs(x==0) << std::endl;
-    std::cout << "In point start = " << input_expr.simplify_indexed().evalf().subs(x==0) << std::endl;
+    GiNaC::Digits = 5; // Константа, определяющая точность
+    if (args.rank == 0){
+        std::cout << "Result = " << result.simplify_indexed().evalf().expand() << std::endl;
+        std::cout << "In point = " << result.simplify_indexed().evalf().subs(args.x==0) << std::endl;
+        std::cout << "In point start = " << args.input_expr.simplify_indexed().evalf().subs(args.x==0) << std::endl;
+    }
     return 0;
 }
