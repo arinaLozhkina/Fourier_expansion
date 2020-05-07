169b521e0f21a0e8679d25b922275cbe57c3ee8d
arinaLozhkina <32067956+arinaLozhkina@users.noreply.github.com>
2020-05-07 22:35:11 +0300

    Update main_launch.cpp

--- main_launch.cpp
+++ main_launch.cpp
@@ -9,46 +9,57 @@
 #include <ctype.h>
 
 #include "fourrier.h"
-using namespace GiNaC;
 
 int main(int argc, char * argv[]){
-    symbol x("x"); // Переменная
-
-    float int_start = -3.141592;
-    float int_end = 3.141592;
+    GiNaC::symbol x("x"); // Переменная
     int rank, size;
     char input_char[50];
     
-    int st = MPI_Init(&argc, &argv);
-    MPI_Status status;
-    if (st != MPI_SUCCESS)
+    int err = MPI_Init(&argc, &argv);
+    if (err != MPI_SUCCESS)
         MPI_Abort(MPI_COMM_WORLD, 1);
     if (MPI_Comm_size(MPI_COMM_WORLD, &size) != MPI_SUCCESS)
         MPI_Abort(MPI_COMM_WORLD, 2);
     if (MPI_Comm_rank(MPI_COMM_WORLD, &rank) != MPI_SUCCESS)
         MPI_Abort(MPI_COMM_WORLD, 3);
     
+    double start_run_time, end_run_time;
+    MPI_Barrier(MPI_COMM_WORLD);
+    start_run_time = MPI_Wtime();
+    
     if (rank == 0){
         std::string input = exp_input(x);
-        std::copy(input.begin(), input.end(), input_char);
+        std::strcpy (input_char, input.c_str());
         if (input == "0"){
-            st = MPI_Finalize();
-               if (st != MPI_SUCCESS)
+            err = MPI_Finalize();
+               if (err != MPI_SUCCESS)
                    MPI_Abort(MPI_COMM_WORLD, 6);
             return 0;
         }
     }
-    st = MPI_Bcast(input_char, 50, MPI_CHAR, 0, MPI_COMM_WORLD);
-    if (st != MPI_SUCCESS)
+    err = MPI_Bcast(input_char, 50, MPI_CHAR, 0, MPI_COMM_WORLD);
+    if (err != MPI_SUCCESS)
         MPI_Abort(MPI_COMM_WORLD, 4);
-    symtab table;
+    GiNaC::symtab table;
     table["x"] = x;
-    parser reader(table);
-    ex input_exp = reader(input_char);
+    GiNaC::parser reader(table);
+    GiNaC::ex input_exp = reader(input_char);
     
-    int new_m = fourrier_parallel(x, input_exp, int_start, int_end);
-    st = MPI_Finalize();
-    if (st != MPI_SUCCESS)
+    float int_start = -3.141592;
+    float int_end = 3.141592;
+    int n = 10; // Число выделяемых гармоник
+    float precision = 0.001; // Точность, при которой коэффициенты не считаем равными 0
+    int n_int = 10001; // Число отрезков разбиения при интегрировании
+    Integral_arguments args{n, precision, n_int, x, input_exp, int_start, int_end, rank, size};
+    int new_m = fourrier_parallel(args);
+    
+    MPI_Barrier(MPI_COMM_WORLD);
+    end_run_time = MPI_Wtime();
+    err = MPI_Finalize();
+    if (err != MPI_SUCCESS)
         MPI_Abort(MPI_COMM_WORLD, 6);
+    
+    if (rank == 0)
+        std::cout << "Program run time: " << end_run_time - start_run_time << std::endl;
     return 0; 
 }
