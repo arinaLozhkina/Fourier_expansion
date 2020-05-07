d800c2105375ed7c96777ca3a23ee2f3c8b36f14
arinaLozhkina <32067956+arinaLozhkina@users.noreply.github.com>
2020-05-07 22:22:07 +0300

    Update fourrier.h

--- fourrier.h
+++ fourrier.h
@@ -11,10 +11,11 @@
 #include <string>
 #include <vector>
 
-using namespace GiNaC;
 
-float compute_integral(ex integral_exp, symbol x, float start_int, float end_int);
+struct Integral_arguments {int n; float precision; int n_int; GiNaC::symbol x; GiNaC::ex input_expr; float int_start; float int_end; int rank; int size;};
+
+float compute_integral(int n, GiNaC::ex integral_exp, GiNaC::symbol x, float start_int, float end_int, int rank, int size);
 std::string exp_input(symbol x);
-int fourrier_parallel(symbol x, ex input_expr, float int_start, float int_end);
+int fourrier_parallel(Integral_arguments args);
 
 #endif /* fourrier_h */
